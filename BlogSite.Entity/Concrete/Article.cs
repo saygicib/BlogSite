67a728f3d60268e94328317f9ce676b190ea0eea
buğrahan saygıcı <bsaygici@hotmail.com>
2022-03-02 18:38:11 +0300

    ..

---
+++ BlogSite.Entity/Concrete/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSite.Entities.Concrete
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ContentSummary { get; set; }
+        public string MainContent { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
