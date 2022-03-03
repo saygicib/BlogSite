2921d1f9d06c27e91f01f983014d47a9eca854ba
Buğrahan <bsaygici@hotmail.com>
2022-03-03 20:57:24 +0300

    Updated database

---
+++ BlogSite.DataAccess/Migrations/20220303175022_updated_UpdatedDate_in_Articles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogSite.DataAccess.Migrations
+{
+    public partial class updated_UpdatedDate_in_Articles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Articles",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Articles",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+        }
+    }
+}
